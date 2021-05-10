cddab24f2cb4987958c30b75b5405b3342eb9c00
deli-hub <deli8908@gmail.com>
2021-05-10 23:16:08 +0900

    Remember names in local Storage

---
+++ chrome-clone/src/greeting.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('.js-form'),
+    input = form.querySelector('input'),
+    greeting = document.querySelector('.js-greetings');
+
+const USER_LS = 'currentUser',
+    SHOWING_CN = 'showing';
+
+function init() {
+    loadName();
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener('submit', handleSubmit);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const inputValue = input.value;
+    localStorage.setItem(USER_LS, inputValue);
+    paintGreeting(inputValue);
+}
+
+init();
